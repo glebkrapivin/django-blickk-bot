6ea296694f4cc6c947f5f769a23b5a14b7500d45
kolyanty <n.pliev@nlogic.ai>
2021-01-17 00:25:33 +0300

    fix allowed hosts

--- django_blickk_bot/settings.py
+++ django_blickk_bot/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'yu-8rgbn!#g+7e_85^(vi8^-@5flt3f&57827f)l0g=+np)9z8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
